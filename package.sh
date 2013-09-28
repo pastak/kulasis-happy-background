bfa77d470cec4b1237dc4ea1c78206aa66bf8fc7
pastak <pasta0915@gmail.com>
2013-09-29 01:56:25 +0900

    zip化するときのディレクトリ階層を修正

--- package.sh
+++ package.sh
@@ -1,1 +1,1 @@
-zip  -r ./kulasis-happy-background.zip ../kulasis-happy-background -x ../kulasis-happy-background/kulasis-happy-background.zip ../kulasis-happy-background/.git\* ../kulasis-happy-background/README.md
+zip  -r ./kulasis-happy-background.zip ./* -x ./kulasis-happy-background.zip ./.git\* ./README.md
