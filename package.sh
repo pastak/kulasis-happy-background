d69f11572b2bf2952d57d96beb9fd528a6455750
pastak <pasta0915@gmail.com>
2013-09-29 01:49:02 +0900

    zipにREADMEを含めない

--- package.sh
+++ package.sh
@@ -1,1 +1,1 @@
-zip  -r ./kulasis-happy-background.zip ../kulasis-happy-background -x ../kulasis-happy-background/kulasis-happy-background.zip ../kulasis-happy-background/.git\*
+zip  -r ./kulasis-happy-background.zip ../kulasis-happy-background -x ../kulasis-happy-background/kulasis-happy-background.zip ../kulasis-happy-background/.git\* ../kulasis-happy-background/README.md
